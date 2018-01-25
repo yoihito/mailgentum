7c70c9fdf1f748ea9841214dd63cc34d87cbea47
Vadim Gribanov <gribanov.vadim.e@gmail.com>
2018-01-25 16:58:27 +0300

    Implement animations for page transitions

--- src/containers/Labels/index.js
+++ src/containers/Labels/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import EntitiesList from 'components/EntitiesList';
 import Threads from 'containers/Threads';
 import LabelsService from 'apis/LabelsService';
@@ -25,14 +26,27 @@ export default class Labels extends Component {
     }
 
     render() {
+        const { location } = this.props;
         const { labels } = this.state;
         return (
             <div className="Labels">
                 { labels &&  <EntitiesList items={labels} itemContainer={LabelItem} /> }
-                <Switch>
-                    <Route path="/dashboard/labels/:labelId" component={Threads} />
-                    <Redirect from="/dashboard" to="/dashboard/labels/INBOX"/>
-                </Switch>   
+                <div className="Labels__threads">
+                    <TransitionGroup>
+                        <CSSTransition
+                            key={location.pathname}
+                            timeout={500}
+                            classNames="pageSlider"
+                            mountOnEnter={false}
+                            unmountOnExit={true}
+                        >
+                            <Switch>
+                                <Route path="/dashboard/labels/:labelId" component={Threads} />
+                                <Redirect from="/dashboard" to="/dashboard/labels/INBOX"/>
+                            </Switch>
+                        </CSSTransition>
+                    </TransitionGroup>
+                </div>
             </div>
         );
     }
--- src/containers/Threads/index.js
+++ src/containers/Threads/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import isEqual from 'lodash.isequal';
-import { SlideRight } from 'animate-components';
 import ThreadItem from 'components/ThreadItem';
 import EntitiesList from 'components/EntitiesList';
 import ThreadsService from 'apis/ThreadsService';
@@ -9,18 +8,12 @@ import Shadow from 'components/Shadow';
 import Scrollable from 'components/Scrollable';
 import './index.css';
 
-const SlideRightStyles = {
-    width: '100%', 
-    height: '100%',
-    background: 'white',
-}
-
 class Threads extends React.Component {
 
     constructor(props) {
         super(props);
 
-        this.state = { };
+        this.state = { threads: [] };
     }
 
     componentDidMount() {
@@ -38,47 +31,21 @@ class Threads extends React.Component {
         const threads = await threadsService.listThreads({ labelIds: labelId });
         this.setState({ 
             threads: threads.sort((a,b) => +a.historyId < +b.historyId),
-            animate: true
-        }, () => {
-            setTimeout(() => {
-                this.setState({
-                    animate: false,
-                    oldThreads: this.state.threads,
-                });
-            }, 1000);
         });
     }
 
     render() {
-        const { oldThreads, threads, animate } = this.state;
-        const result = [];
-
-        if (oldThreads) {
-            result.push((<div className="Threads__old">{React.createElement(
-                Shadow(Scrollable(EntitiesList)), {
-                    items: oldThreads, 
-                    itemContainer: ThreadItem,
-                }
-            )}</div>));
-        }
-
-        if (threads && animate) {
-            result.push((
-                <SlideRight style={SlideRightStyles} key={animate}>
-                    {React.createElement(
-                        Shadow(Scrollable(EntitiesList)), {
-                            items: threads, 
-                            itemContainer: ThreadItem,
-                        }
-                    )}
-                </SlideRight>
-            ));
-        }
-
-        return <div className="Threads">
-            {result}
-        </div>;
-        
+        const { threads } = this.state;
+        return (<div className="Threads">
+            {
+                React.createElement(
+                    Shadow(Scrollable(EntitiesList)), {
+                        items: threads,
+                        itemContainer: ThreadItem,
+                    }
+                )
+            }
+        </div>);
     }
 }
 
